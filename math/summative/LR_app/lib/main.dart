723b9a3d2b54b8c32385f4d0a9899b049055e025
Esther <e.mbanzabig@alustudent.com>
2024-07-21 23:21:20 +0200

    doneee

--- math/summative/LR_app/lib/main.dart
+++ math/summative/LR_app/lib/main.dart
@@ -1,23 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:linear_rregression_flutter/dementia_app.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: ' Predicttion App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF8FB73A)),
-        useMaterial3: true,
+      home: PredictionPage(),
+    );
+  }
+}
+
+class PredictionPage extends StatefulWidget {
+  @override
+  _PredictionPageState createState() => _PredictionPageState();
+}
+
+class _PredictionPageState extends State<PredictionPage> {
+  final TextEditingController _controller = TextEditingController();
+  String _prediction = '';
+
+  Future<void> _predict() async {
+    final url = Uri.parse('https://regg-api-vgpo.onrender.com/predict');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'TV': int.parse(_controller.text)}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        _prediction = 'Predicted Sales: ${data['predicted_sales']}';
+      });
+    } else {
+      setState(() {
+        _prediction = 'Error: ${response.reasonPhrase}';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sales Prediction'),
       ),
-      home: const DementiaApp(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: 'TV Advertising Budget'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _predict,
+              child: Text('Predict'),
+            ),
+            SizedBox(height: 20),
+            Text(_prediction),
+          ],
+        ),
+      ),
     );
   }
 }
