09fa263657c6d009cc015d1a2c25a58e88c1c943
Esther-Mbanzabigwi <e.mbanzabig@alustudent.com>
2024-10-23 04:48:29 -0700

    ApI

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,41 +1,25 @@
 #!/usr/bin/env python3
 
-"""Script that prints the location of a specific user"""
 
-#!/usr/bin/env python3
-
-"""Script that prints the location of a specific user"""
+""" Return list of ships"""
 
 import requests
 import sys
 import time
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./2-user_location.py <URL>")
-        sys.exit(1)
+    res = requests.get(sys.argv[1])
 
-    try:
-        res = requests.get(sys.argv[1])
+    if res.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        # get remaining rate
 
-        if res.status_code == 403:
-            rate_limit = int(res.headers.get('X-Ratelimit-Reset', 0))
-            current_time = int(time.time())
-            diff = int((rate_limit - current_time) / 60)
-            print('Reset in {} min'.format(diff))
-
-        elif res.status_code == 404:
-            print("Not found")
-        elif res.status_code == 200:
-            res = res.json()
-            # Check if 'location' key exists
-            if 'location' in res:
-                print(res['location'])
-            else:
-                print("Location not found in the response")
-        else:
-            print("Unexpected status code:", res.status_code)
-
-    except requests.exceptions.RequestException as e:
-        print("Error fetching the URL:", e)
-
+    elif res.status_code == 404:
+        print("Not found")
+    elif res.status_code == 200:
+        res = res.json()
+        print(res['location'])
